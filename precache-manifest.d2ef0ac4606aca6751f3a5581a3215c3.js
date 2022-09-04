d688ff5f7bf080903c15882fb1b26b9b150db527
Tob <tobiadonati@gmail.com>
2022-09-04 12:29:07 +0100

    Updates

---
+++ precache-manifest.d2ef0ac4606aca6751f3a5581a3215c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a879abb77b5d48064d43",
+    "url": "/static/css/main.6703b702.chunk.css"
+  },
+  {
+    "revision": "a879abb77b5d48064d43",
+    "url": "/static/js/main.62cedf46.chunk.js"
+  },
+  {
+    "revision": "2a2beed754bc93051296",
+    "url": "/static/js/runtime~main.8fdbeed6.js"
+  },
+  {
+    "revision": "f5c77af55025510d8bda",
+    "url": "/static/js/2.00e84f46.chunk.js"
+  },
+  {
+    "revision": "181b108a7e6d4571657c",
+    "url": "/static/js/3.bfcf3618.chunk.js"
+  },
+  {
+    "revision": "8b0f39b22970be03a27911fadcb49b54",
+    "url": "/index.html"
+  }
+];
