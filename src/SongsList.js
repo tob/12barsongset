b24bd16c66121c87d570f81ee141b2772d819ad3
Tob <tobiadonati@gmail.com>
2022-09-04 12:26:52 +0100

    Add react and songlist

---
+++ src/SongsList.js
@@ -0,0 +1,174 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {
+  Stack,
+  Card,
+  CardHeader,
+  Avatar,
+  CardActionArea,
+  IconButton,
+} from "@mui/material";
+import { ArrowDropDown, ArrowDropUp } from "@mui/icons-material";
+import { Song } from "./components/Song";
+
+// SERVICES THAT CALL OUR API ENDPOINTS
+import { getAllSongs } from "./services/profileService";
+
+const SongList = () => {
+  const [songs, setSongs] = useState(null);
+  const [dragId, setDragId] = useState();
+
+  const handleDrag = (ev) => {
+    console.log(ev.currentTarget.id);
+    setDragId(ev.currentTarget.id);
+  };
+
+  function moveElement(from, to) {
+    const newSongsOrder = songs.reduce((prev, current, idx, self) => {
+      if (from === to) {
+        prev.push(current);
+      }
+      if (idx === from) {
+        return prev;
+      }
+      if (from < to) {
+        prev.push(current);
+      }
+      if (idx === to) {
+        prev.push(self[from]);
+      }
+      if (from > to) {
+        prev.push(current);
+      }
+      return prev;
+    }, []);
+    setSongs(newSongsOrder);
+  }
+
+  const handleDrop = (ev) => {
+    const dragSong = songs.find((song) => song._id === dragId);
+    const dropSong = songs.find((song) => song._id === ev.currentTarget.id);
+
+    const dragSongOrder = dragSong.order;
+    const dropSongOrder = dropSong.order;
+
+    const newSongsState = songs.map((song) => {
+      if (song._id === dragId) {
+        song.order = dropSongOrder;
+      }
+      if (song._id === ev.currentTarget.id) {
+        song.order = dragSongOrder;
+      }
+      return song;
+    });
+    newSongsState.sort((a, b) => {
+      return a.order - b.order;
+    });
+
+    console.log(newSongsState);
+    setSongs(newSongsState);
+  };
+
+  useEffect(() => {
+    async function getSongs() {
+      if (!songs) {
+        const response = await getAllSongs();
+        response.sort((a, b) => {
+          return a.order - b.order;
+        });
+        setSongs(response);
+      }
+    }
+
+    getSongs();
+  }, [songs]);
+
+  return (
+    <>
+      <Router basename={"/tracklist-viewer"}>
+        <Routes>
+          <Route
+            path="/about"
+            element={
+              <div>
+                <h2>RussMatazz</h2>
+                <p>This is a little web app to handle our tracklist</p>
+              </div>
+            }
+          />
+          <Route
+            path="/:songId"
+            element={
+              songs && songs.length > 0 ? (
+                <Song songs={songs} />
+              ) : (
+                "no songs yet"
+              )
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <Stack spacing={2}>
+                {songs && songs.length > 0 ? (
+                  songs.map((song, index) => (
+                    <Card
+                      id={song._id}
+                      key={song._id}
+                      draggable={true}
+                      onDragOver={(ev) => ev.preventDefault()}
+                      onDragStart={handleDrag}
+                      onTouchStart={handleDrag}
+                      onDrop={handleDrop}
+                      onTouchEnd={handleDrop}
+                      sx={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "stretch",
+                      }}
+                    >
+                      {index > 0 && (
+                        <CardActionArea
+                          onClick={() => moveElement(index, index - 1)}
+                          sx={{
+                            width: "auto",
+                            display: "flex",
+                            alignItems: "center",
+                          }}
+                        >
+                          <ArrowDropUp />
+                        </CardActionArea>
+                      )}
+                      <CardActionArea component={Link} to={`/${song._id}`}>
+                        <CardHeader
+                          title={song.title}
+                          subheader={song.version}
+                        />
+                      </CardActionArea>
+                      {index < songs.length - 1 && (
+                        <CardActionArea
+                          onClick={() => moveElement(index, index + 1)}
+                          sx={{
+                            width: "auto",
+                            display: "flex",
+                            alignItems: "center",
+                          }}
+                        >
+                          <ArrowDropDown />
+                        </CardActionArea>
+                      )}
+                    </Card>
+                  ))
+                ) : (
+                  <p>No songs found</p>
+                )}
+              </Stack>
+            }
+          />
+        </Routes>
+      </Router>
+    </>
+  );
+};
+
+export default SongList;
---
+++ src/components/Song.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { Stack, Typography, Fab } from "@mui/material";
+
+import { useParams } from "react-router-dom";
+import { Navigation } from "./Navigation";
+
+export const Song = ({ songs }) => {
+  let { songId } = useParams();
+  const song = songs.find((song) => song._id === songId);
+  const [fontSize, setFontSize] = useState(1);
+  const currentIndex = songs.indexOf(song) || 0;
+  const prevSong =
+    currentIndex > 0 ? songs[currentIndex - 1] : songs[songs.length - 1];
+
+  const nextSong =
+    currentIndex < songs.length - 1 ? songs[currentIndex + 1] : songs[0];
+
+  const formattedLyrics =
+    song && song.lyrics && song.lyrics.replace(/\\n/g, "<br />\n");
+
+  const increaseFontSize = () => {
+    setFontSize(fontSize + 0.1);
+  };
+
+  const decreaseFontSize = () => {
+    setFontSize(fontSize - 0.1);
+  };
+
+  return song && song.title ? (
+    <div>
+      <Typography
+        variant="body1"
+        sx={{
+          columns: `4 ${300 * fontSize}px`,
+          columnGap: "4em",
+          fontSize: `${fontSize}em`,
+        }}
+        dangerouslySetInnerHTML={{ __html: formattedLyrics }}
+      ></Typography>
+      <Navigation
+        prev={{ title: prevSong.title, id: prevSong._id }}
+        next={{ title: nextSong.title, id: nextSong._id }}
+        increaseFontSize={increaseFontSize}
+        decreaseFontSize={decreaseFontSize}
+        title={song.title}
+      />
+    </div>
+  ) : null;
+};
